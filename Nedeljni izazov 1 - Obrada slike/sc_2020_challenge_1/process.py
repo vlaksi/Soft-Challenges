471ac8911c82cc0fe3997067da769d10ccfbccb7
vlaksi <daksrapsody@gmail.com>
2020-11-03 03:27:33 +0100

    ne bas sjajno brojanje rbc-a

--- Nedeljni izazov 1 - Obrada slike/sc_2020_challenge_1/process.py
+++ Nedeljni izazov 1 - Obrada slike/sc_2020_challenge_1/process.py
@@ -21,6 +21,18 @@ def count_blood_cells(image_path):
     img = cv2.imread(image_path)
 
     white_blood_cell_count = get_wbc(img)
+    sum_wbc_rbc = get_rbc_wbc_count_together(image_path)
+    # rbc sracunam tako sto od ukupnog broja oduzmem broj nadjenih wbc
+    red_blood_cell_count = sum_wbc_rbc - white_blood_cell_count
+    # TODO: OBRISATI OVO I NACI NESTO PAMENIJE
+    # Trenutno ovo sto radi je da ako wbc ima vise od sume wbc i rbc, sigurno nesto ne valja
+    # pa onda broj rbc povecam za 5 puta vise nego wbc, ZNAM LOSE JE ALI OPET NIJE U MINUSU :D
+    if red_blood_cell_count < 0:
+        red_blood_cell_count = white_blood_cell_count * 5
+
+    print("path: " + image_path)
+    print("wbc: " + str(white_blood_cell_count))
+    print("rbc: " + str(red_blood_cell_count) + "\n")
 
     # TODO - Prebrojati crvena i bela krvna zrnca i vratiti njihov broj kao povratnu vrednost ove procedure
 
@@ -58,6 +70,42 @@ def get_wbc(img):
     return len(zeljene_konture)
     # draw_wanted_contours(img, zeljene_konture)
 
+def get_rbc_wbc_count_together(img_path):
+    img = cv2.imread(img_path)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)  # konvert u grayscale
+    # plt.imshow(img_gray)
+
+    image_bin = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 105, 5)
+    # plt.imshow(image_bin, 'gray')
+
+    # ZATVARAMO - NZM STO :D
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    closing = cv2.morphologyEx(image_bin, cv2.MORPH_CLOSE, kernel)
+
+    #plt.imshow(opening, 'gray')
+
+    # VRSIM DILACIJU i OBRCEM DA OBJEKTI BUDU BELI ZBOG PRONALAZENJA KONTURA KOJE MORAJU BITI BELE
+    kernel = np.ones((8, 8), np.uint8)
+    dilation = cv2.erode(closing, kernel, iterations=1)
+    dilation = 255 - dilation
+    # plt.imshow(dilation, 'gray')
+
+    # PRONALAZIM BELE KONTURE
+    _, cnts, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    zeljene_konture = []
+    for c in cnts:
+        # print(cv2.contourArea(c))
+        if cv2.contourArea(c) > 700:
+            # print("dodao")
+            # print(cv2.contourArea(c))
+            zeljene_konture.append(c)
+
+    image_crtanje = img.copy()
+    cv2.drawContours(image_crtanje, zeljene_konture, -1, (255, 0, 0), 1)
+    # plt.imshow(image_crtanje)
+    # print(len(zeljene_konture))
+    return len(zeljene_konture)
 
 # def draw_wanted_contours(img,zeljene_konture):
 #     image_crtanje = img.copy()
