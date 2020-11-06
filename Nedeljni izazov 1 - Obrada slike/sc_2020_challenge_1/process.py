80fed760cf7ae9b1bb6f8aec5b108ce8c3d5a194
vlaksi <daksrapsody@gmail.com>
2020-11-06 03:19:46 +0100

    [tacnost: 55%] Verzija optimizovanog trazenja samo kontura na osnovu watershed algoritma, optimizovana u smislu da sam prebacio na kanal koji odgovara i losijem i boljem kvalitetu slike

--- Nedeljni izazov 1 - Obrada slike/sc_2020_challenge_1/process.py
+++ Nedeljni izazov 1 - Obrada slike/sc_2020_challenge_1/process.py
@@ -21,7 +21,7 @@ def count_blood_cells(image_path):
     img = cv2.imread(image_path)
 
     white_blood_cell_count = get_wbc(img)
-    red_blood_cell_count = get_rbc_with_watershed_low_quality_image(image_path)
+    red_blood_cell_count = get_rbc_with_watershed_optimized(image_path)
 
     print("path: " + image_path)
     print("rbc: " + str(red_blood_cell_count))
@@ -32,8 +32,8 @@ def count_blood_cells(image_path):
     print("procenat wbc: " + str(procenat) + "\n")
 
     # EDGE CASEOVI ZA SLUCAJ DA MI NE RADI ALGORITAM
-    if red_blood_cell_count <= 4:
-        red_blood_cell_count = white_blood_cell_count * 5
+    # if red_blood_cell_count <= 4:
+    #     red_blood_cell_count = white_blood_cell_count * 5
 
     # TODO: NADJI PAMETNIJU PROVERU DA LI IMA ILI NEMA LEUKEMIJU
     if procenat > 8.0:
@@ -277,6 +277,68 @@ def get_rbc_with_watershed_low_quality_image(img_path):
 
     return len(cnts)
 
+
+def get_rbc_with_watershed_optimized(img_path):
+    img = cv2.imread(img_path)
+    image = img.copy()
+
+    # MENJAM U KANAL POGODNIJI ZA DETEKTOVANJE RBC
+    gray = image[:, :, 0]
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # plt.imshow(thresh, 'gray')
+
+    # noise removal
+    kernel = np.ones((5, 5), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+    # plt.imshow(opening, 'gray')
+
+    # sure background area
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
+    # plt.imshow(sure_bg, 'gray')
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,3)
+    ret, sure_fg = cv2.threshold(dist_transform,0.1*dist_transform.max(),255,0)
+    # plt.imshow(sure_fg,'gray')
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg,sure_fg)
+
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(sure_fg)
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers + 10
+    # Now, mark the region of unknown with zero
+    markers[unknown == 255] = 0
+    # plt.imshow(markers)
+
+    markers = cv2.watershed(img, markers)
+    img[markers == -1] = [255, 0, 0]
+
+    # GRANICE SU OZNACENE SA -1 PA IM DODELJUJEM BOJU
+    img1 = img.copy()
+    # print(markers)
+    img1[markers > 10] = [0, 0, 0]
+    img1[markers <= 10] = [255, 255, 255]
+    # img1[markers == -1] = [0,0,0]
+    # plt.imshow(img1)
+
+    # PRETVARAM IZ WATERSHEDA U KORISNU SLIKU ZA TRAZENJE KONTURA
+    img = cv2.cvtColor(img1.copy(), cv2.COLOR_BGR2GRAY)
+    ret, image_bin = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY)  # ret je vrednost praga, image_bin je binarna slika
+    image_bin = image_bin - 255
+    # plt.imshow(image_bin, 'gray')
+
+    # PRONALAZIM BELE KONTURE
+    _, cnts, _ = cv2.findContours(image_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    print("ukupno kontura: " + str(len(cnts)))
+
+    image_crtanje = image.copy()
+    cv2.drawContours(image_crtanje, cnts, -1, (255, 0, 0), 2)
+    # plt.imshow(image_crtanje)
+
+    return len(cnts)
 
 def get_limits_for_wanted_color(wanted_bgr_color):
     wanted_color = np.uint8([[wanted_bgr_color]])  # here insert the bgr values which you want to convert to hsv
